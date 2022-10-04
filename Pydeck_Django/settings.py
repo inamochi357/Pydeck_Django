fd5a5f8fb2cec31d9c11b398cd9e8d463f1db109
inamochi <huyu3184@gmail.com>
2022-10-04 13:15:34 +0900

    settings update

--- Pydeck_Django/settings.py
+++ Pydeck_Django/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-from .settings_secret import SECRET_KEY
+try:
+    from .settings_secret import SECRET_KEY
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
